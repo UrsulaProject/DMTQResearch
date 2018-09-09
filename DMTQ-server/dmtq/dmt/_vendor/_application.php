860093615245c01953b33b46f8ea553b695a2583
tungnotpunk <tungnotpunk@gmail.com>
2018-09-10 01:31:04 +0800

    updated

---
+++ DMTQ-server/dmtq/dmt/_vendor/_application.php
@@ -0,0 +1,31 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+function runApi ($method) {
+    $controllerName = ucfirst(substr($method, 0, strpos($method, '.')));
+    $actionName = substr($method, strpos($method, '.') + 1);
+    if (class_exists($controllerName) && method_exists($controllerName, $actionName)) {
+        $controller = new $controllerName();
+        return $controller->$actionName($_POST);
+    } else {
+        return (object)[
+            'result' => [],
+            'error' => NULL
+        ];
+    }
+    return (object)[];
+
+}
+function runSystem ($method) {
+    header('Content-Type: application/json');
+    echo json_encode(runApi($method));
+}
+function getHeader($headerId, $defaultValue) {
+    $headerList = apache_request_headers();
+    foreach ($headerList as $name => $value) {
+        if ($headerId === $name) {
+            return $value;
+        }
+    }
+    return $defaultValue;
+}
